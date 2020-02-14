050e9a785ce85361988fbbabc3a04939b4d1d39a
Alexey Sorokin <aso@inmedias.it>
2020-02-14 09:47:50 +0100

    confparser added, core implementation ready

--- get_ad_users.py
+++ get_ad_users.py
@@ -1,14 +1,16 @@
 #! /bin/env python3
 import sys
 import ldap
+import configparser
 
-ad_server = "10.10.10.10"
-ad_user_basedn = "dc=chaos,dc=inmedias,dc=it"
-ad_user_filter = '(&(objectClass=USER)(sAMAccountName={username}))'
-ad_userdn_filter = '(&(objectClass=USER)(dn={userdn}))'
-ad_group_filter = '(&(objectClass=GROUP)(cn={group_name}))'
-ad_bind_usr = 'Kopano Agent'
-ad_bind_pwd = '10027a8a6637cae33a933535e2dad2cd'
+# Configparser
+conf = configparser.ConfigParser()
+conf.read('Config.ini')
+ad_server = conf['ldap-config']['ad_server']
+ad_user_basedn = conf['ldap-config']['ad_user_basedn']
+ad_member_of = conf['ldap-config']['ad_member_of']
+ad_bind_usr = conf['ldap-config']['ad_bind_usr']
+ad_bind_pwd = conf['ldap-config']['ad_bind_pwd']
 
 # Connect
 def ldap_auth (usr = ad_bind_usr, pwd = ad_bind_pwd, address = ad_server):
@@ -33,22 +35,32 @@ def ldap_auth (usr = ad_bind_usr, pwd = ad_bind_pwd, address = ad_server):
 # get group members
 def get_members(groupname, ldap_conn, basedn = ad_user_basedn):
     members = []
-    #ad_filter = ad_group_filter.replace('{group_name}', groupname)
-    ad_filter = '(&(objectClass=USER)(sAMAccountName=*)(memberOf=cn=Alle,OU=Kopano,dc=chaos,dc=inmedias,dc=it))'
+    members.append(f'[{groupname}]')
+    ad_filter = f'(&(objectClass=USER)(sAMAccountName=*){ad_member_of}(memberOf=cn=Alle,OU=Kopano,dc=chaos,dc=inmedias,dc=it))'
     result = ldap_conn.search_s(basedn, ldap.SCOPE_SUBTREE, ad_filter )
     if result:
         #print (result[0])
         for user, attrb in result:
             if 'mail' in attrb:
-                members.append(str(attrb['mail'][0]))
+                tmp_mail = ""
+                tmp_cn = ""
+                attr_mail = str(attrb['mail'][0])
+                attr_cn = str(attrb['cn'][0])
+                for i in range(0, len(attr_mail)):
+                    if i != 0:
+                        tmp_mail = tmp_mail + attr_mail[i]
+                for i in range(0, len(attr_cn)):
+                    if i !=0:
+                        tmp_cn = tmp_cn + attr_cn[i]
+                members.append(tmp_mail + "=" + tmp_cn)
                 
     return members
 
 
 if __name__ == "__main__":
-  group_name = sys.argv[1]
+  groupname = sys.argv[1]
   ldap_conn, result = ldap_auth()
   if result:
-    group_members = get_members(group_name, ldap_conn)
+    group_members = get_members(groupname, ldap_conn)
     for m in group_members:
         print(m)
